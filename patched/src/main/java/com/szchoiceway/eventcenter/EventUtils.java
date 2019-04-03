156165f513c5b6818bcd26c2142ccc05dcd26ac0
Jan-Willem Spuij <jw@spuij.nl>
2019-04-03 16:23:45 +0200

    Initial commit of patch project

---
+++ patched/src/main/java/com/szchoiceway/eventcenter/EventUtils.java
@@ -0,0 +1,16 @@
+package com.szchoiceway.eventcenter;
+
+import android.util.Log;
+import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexReplace;
+import java.io.IOException;
+
+@DexEdit
+public class EventUtils {
+
+    @DexReplace
+    public static void killProcess(String packageName) {
+      Log.i("killProcess", "closeApplication patched " + packageName);
+    }
+
+}
