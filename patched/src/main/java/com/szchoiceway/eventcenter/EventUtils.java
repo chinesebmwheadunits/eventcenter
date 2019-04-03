e44fa70d51e3d6745a3e020dd8ad84feda615af3
Jan-Willem Spuij <jw@spuij.nl>
2019-04-03 22:55:12 +0200

    Missed annotation for internal eSrcMode enum.

--- patched/src/main/java/com/szchoiceway/eventcenter/EventUtils.java
+++ patched/src/main/java/com/szchoiceway/eventcenter/EventUtils.java
@@ -2,6 +2,7 @@ package com.szchoiceway.eventcenter;
 
 import android.util.Log;
 import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexIgnore;
 import lanchon.dexpatcher.annotation.DexReplace;
 import java.io.IOException;
 
@@ -11,6 +12,11 @@ public class EventUtils {
     @DexReplace
     public static void killProcess(String packageName) {
       Log.i("killProcess", "closeApplication patched " + packageName);
+    }
+
+    @DexIgnore
+    public enum eSrcMode {
+
     }
 
 }
